d842b3deef155294444486636181b45e3cb400e6
LamVyHuynh <huynhlamvy145@gmail.com>
2025-02-07 09:27:03 +0700

    da lam xong thao tac header mo menu o tablet va mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var toolbar = document.getElementById("toolBar");
 var nav = document.querySelector(".nav");
+nav.classList.add("dp_none");
 
 toolbar.onclick = function () {
   nav.classList.toggle("dp_none");
