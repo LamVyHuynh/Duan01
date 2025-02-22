01812cb4b5e397c906759fa260341f5d18e161d4
LamVyHuynh <huynhlamvy145@gmail.com>
2025-02-22 15:31:02 +0700

    da dem dc navbar left vao nhung chua lam duoc cai de khi scroll no khi bi cuon di mat, sua lai nut, logo thi bang refresh

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var toolbar = document.getElementById("toolBar");
-var nav = document.querySelector(".nav");
-nav.classList.add("dp_none");
-
-toolbar.onclick = function () {
-  nav.classList.toggle("dp_none");
-};
