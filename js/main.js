099a3ebcad9302faad572168a81d19f419141c83
LamVyHuynh <huynhlamvy145@gmail.com>
2025-02-26 16:44:41 +0700

    xong phan hien frame dang ky

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ setInterval(() => {
   index = (index % totalSlide) + 1;
 }, time);
 // Kết thúc chuyển slide tự động trong banner
+
+// Hiện frame đăng ký, đăng nhập
+const dki = document.querySelector(".btn_dangki");
+const close = document.querySelector(".icon-close");
+const dki_dnhap = document.querySelector("#dki_dnhap");
+dki.addEventListener("click", function () {
+  dki_dnhap.classList.remove("close");
+});
+
+close.addEventListener("click", function () {
+  dki_dnhap.classList.add("close");
+});
+// Kết thúc hiện frame đăng ký, đăng nhập
