7b7c4e399b66b43f31d74c6de69e120ab698e170
LamVyHuynh <huynhlamvy145@gmail.com>
2025-02-27 08:39:49 +0700

    them chuyen dong de hien khung dang ki

--- js/main.js
+++ js/main.js
@@ -11,13 +11,28 @@ setInterval(() => {
 
 // Hiện frame đăng ký, đăng nhập
 const dki = document.querySelector(".btn_dangki");
-const close = document.querySelector(".icon-close");
+const closeFrame = document.querySelector(".icon-close");
 const dki_dnhap = document.querySelector("#dki_dnhap");
-dki.addEventListener("click", function () {
+const frameDki = document.querySelector(".js_frame-dki");
+
+// Hàm mở khung đăng kí tài khoản
+function open_dki() {
   dki_dnhap.classList.remove("close");
+}
+
+dki.addEventListener("click", open_dki);
+
+// Hàm đóng khung đăng kí tài khoản
+function close_dki() {
+  dki_dnhap.classList.add("close");
+}
+
+closeFrame.addEventListener("click", close_dki);
+dki_dnhap.addEventListener("click", close_dki);
+
+// Không cho nổ bọt bấm bên ngoài thì close nhưng vào bên trong khung thì không close
+frameDki.addEventListener("click", function (event) {
+  event.stopPropagation();
 });
 
-close.addEventListener("click", function () {
-  dki_dnhap.classList.add("close");
-});
 // Kết thúc hiện frame đăng ký, đăng nhập
